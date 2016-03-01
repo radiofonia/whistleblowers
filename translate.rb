84f20d796b5d494c37307422d7ce43f3a2054176
Marcin Koziej <marcin@cahoots.pl>
2016-03-01 12:47:33 +0100

    translate for all targets

--- translate.rb
+++ translate.rb
@@ -1,7 +1,6 @@
 require 'nokogiri'
 require 'csv'
 
-target= 'czech'
 
 trans = CSV.read("translate.csv")
 headers = trans[0]
@@ -15,25 +14,26 @@ trans[1..-1].each do |phrase|
   translations[phrase[0]] = p
 end
 
-dom = Nokogiri::HTML.parse open("index.html").read()
+headers[1..-1].each do |target|
+  dom = Nokogiri::HTML.parse open("index.html").read()
 
-open("strings.en.txt", "w") do |stringsOut|
-  dom.xpath('//text()').map do |txt|
-    s = txt.content
-    s = s.strip
-    next if s == ''
-    stringsOut.write s + "\n"
-    
-    t = translations[s]
-    if t.nil?
-      puts "ERROR! no text for #{s}"
-    else
-      puts "#{t}==#{t[target]}"
-      txt.content = t[target]
+  open("strings.en.txt", "w") do |stringsOut|
+    dom.xpath('//text()').map do |txt|
+      s = txt.content
+      s = s.strip
+      next if s == ''
+      stringsOut.write s + "\n"
+      
+      t = translations[s]
+      if t.nil?
+        puts "ERROR! no text for #{s}"
+      else
+        puts "#{t}==#{t[target]}"
+        txt.content = t[target]
+      end
     end
   end
+
+
+  dom.write_html_to(File.new("index.#{target}.html", 'w'), :encoding => 'UTF-8')
 end
-
-puts dom.to_html
-
-dom.write_html_to(File.new("index.#{target}.html", 'w'), :encoding => 'UTF-8')
